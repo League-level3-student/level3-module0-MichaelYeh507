e7fa2cb0f595ba610ca6ffd11358d7901588e150
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2020-06-14 11:44:59 -0700

    MoreArrayFun (finished)

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,43 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+		String[] greetings = {"hi", "hello", "hey"};
+		random(greetings);
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	public static void print(String[] string) {
+		for (int i = 0; i < string.length; i++) {
+			System.out.println(string[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	public static void reverse(String[] string) {
+		for (int i = string.length -1; i < string.length; i--) {
+			System.out.println(string[i]);
+		}
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	public static void everyOther(String[] string) {
+		for (int i = 0; i < string.length; i+=2) {
+			System.out.println(string[i]);
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	public static void random(String[] string) {
+		Random rand = new Random();
+		for (int i = 0; i < string.length; i += rand.nextInt(string.length)) {
+			System.out.println(string[i]);
+		}
+	}
 	
 }
